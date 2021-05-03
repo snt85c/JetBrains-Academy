dcb16d74aca280ea5479610563cf34262a35b91d
snt85c <66259347+snt85c@users.noreply.github.com>
2021-05-03 11:41:35 +0100

    Update Main.java

--- Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe with AI/task/src/tictactoe/Main.java
@@ -12,8 +12,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         userInterface(scanner);
     }
-    wk
-:wq
 
     public static void userInterface (Scanner scanner){
         System.out.println("Enter the cells: ");
